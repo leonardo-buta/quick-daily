2bfa941d1a31a7a2d7dce4c75bee1bf0ee1755a4
Leonardo Buta <leonardo.lbuta@gmail.com>
2021-01-16 20:08:35 -0300

    Steps creation

--- js/script.js
+++ js/script.js
@@ -2,46 +2,88 @@ $(document).ready(function () {
 
     // SmartWizard initialize
     $('#smartwizard').smartWizard({
-        theme: 'progress',
+        theme: 'arrows',
+        autoAdjustHeight: false,
+        enableURLhash: false,
+        leaveStep: function () { alert(); },
+        transition: {
+            animation: 'slide-swing',
+            speed: '400',
+        },
         lang: {
             next: 'Próximo',
             previous: 'Voltar'
         }
     });
 
+    $("#smartwizard").on("leaveStep", function (e, anchorObject, currentStepIndex, nextStepIndex, stepDirection) {
+        let shouldAdvanceStep = true;
+
+        switch (currentStepIndex) {
+            case 0:
+                loadStep2();
+                break;
+            case 1:
+                shouldAdvanceStep = chooseNextParticipant();
+                break;
+            case 2:
+                loadStep3();
+                break;
+            default:
+                break;
+        }
+
+        return shouldAdvanceStep;
+    });
+
 });
 
 var participants = [];
-
-function goToStep2() {
-    participants = document.getElementById('w3review').value.split('\n').map((item) => {
-        return { participant: item, chosen: false, time: 0 };
-    });
-}
 
 function goToStep3() {
     createResultsTable();
 }
 
-function chooseNext() {
-
+function chooseNextParticipant() {
+    let shouldAdvanceStep = false;
     let notChosenparticipants = participants.filter((item) => { return !item.chosen });
-    let nextParticipant = Math.floor(Math.random() * notChosenparticipants.length);
 
-    let index = participants.findIndex((item) => { return item.participant === notChosenparticipants[nextParticipant].participant });
-    participants[index].chosen = true;
+    if (notChosenparticipants.length > 0) {
+        let nextParticipant = Math.floor(Math.random() * notChosenparticipants.length);
+        let index = participants.findIndex((item) => { return item.participant === notChosenparticipants[nextParticipant].participant });
+        participants[index].chosen = true;
 
-    document.getElementById('currentParticipant').innerHTML = notChosenparticipants[nextParticipant].participant;
+        document.getElementById('currentParticipant').innerHTML = notChosenparticipants[nextParticipant].participant;
+    } else {
+        shouldAdvanceStep = true;
+    }
+
+    return shouldAdvanceStep;
 }
 
 function createResultsTable() {
 
-    let table = document.getElementById('results').getElementsByTagName('tbody')[0];
-    let row = table.insertRow();
+    let tbody = document.getElementById('results').getElementsByTagName('tbody')[0];
+    let row = tbody.insertRow();
 
     for (let data of participants) {
         row.insertCell(0).innerHTML = data.participant;
         row.insertCell(1).innerHTML = data.time;
-        row = table.insertRow();
+        row = tbody.insertRow();
     }
+}
+
+function loadStep2() {
+    saveParticipants();
+    chooseNextParticipant();
+}
+
+function loadStep3() {
+    alert('passo 3');
+}
+
+function saveParticipants() {
+    participants = document.getElementById('w3review').value.split('\n').map((item) => {
+        return { participant: item, chosen: false, time: 0 };
+    });
 }
