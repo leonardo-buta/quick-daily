9c5b3319fd4ad7bdbe5d3e49e3ced1342f37fdfe
Leonardo Buta <leonardo.lbuta@gmail.com>
2021-01-18 01:04:39 -0300

    Added a timer and adjustments

--- js/script.js
+++ js/script.js
@@ -21,6 +21,9 @@ $(document).ready(function () {
             case 1:
                 loadStep2();
                 break;
+            case 2:
+                loadStep3();
+                break;
             default:
                 break;
         }
@@ -30,6 +33,9 @@ $(document).ready(function () {
         let shouldAdvanceStep = true;
 
         switch (currentStepIndex) {
+            case 0:
+                shouldAdvanceStep = checkEmptyParticipants();
+                break;
             case 1:
                 shouldAdvanceStep = chooseNextParticipant();
                 break;
@@ -41,10 +47,16 @@ $(document).ready(function () {
     });
 });
 
-var participants = [];
+let participants = [];
+let startTime;
+let elapsedTime = 0;
+let totalElapsedTime = 0;
+let timerInterval;
+let currentParticipantIndex = 0;
 
-function goToStep3() {
+function loadStep3() {
     createResultsTable();
+    updateTotalElapsed();
 }
 
 function chooseNextParticipant() {
@@ -56,8 +68,19 @@ function chooseNextParticipant() {
         let index = participants.findIndex((item) => { return item.participant === notChosenparticipants[nextParticipant].participant });
         participants[index].chosen = true;
 
+
         document.getElementById('currentParticipant').innerHTML = notChosenparticipants[nextParticipant].participant;
+
+        if (participants.length !== notChosenparticipants.length) {
+            pause(currentParticipantIndex);
+        }
+
+        start();
+
+        currentParticipantIndex = index;
+
     } else {
+        pause();
         shouldAdvanceStep = true;
     }
 
@@ -65,13 +88,14 @@ function chooseNextParticipant() {
 }
 
 function createResultsTable() {
-
     let tbody = document.getElementById('results').getElementsByTagName('tbody')[0];
+    tbody.innerHTML = '';
+
     let row = tbody.insertRow();
 
     for (let data of participants) {
         row.insertCell(0).innerHTML = data.participant;
-        row.insertCell(1).innerHTML = data.time;
+        row.insertCell(1).innerHTML = timeToString(data.time);
         row = tbody.insertRow();
     }
 }
@@ -79,14 +103,58 @@ function createResultsTable() {
 function loadStep2() {
     saveParticipants();
     chooseNextParticipant();
-}
-
-function loadStep3() {
-    alert('passo 3');
+    totalElapsedTime = 0;
 }
 
 function saveParticipants() {
-    participants = document.getElementById('w3review').value.split('\n').map((item) => {
-        return { participant: item, chosen: false, time: 0 };
-    });
+    participants = document.getElementById('participants').value.split('\n')
+        .filter((item) => { return item.trim() !== '' })
+        .map((item) => { return { participant: item.toUpperCase(), chosen: false, time: 0 }; });
+}
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
+
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(2, "0");
+
+    return `${formattedMM}:${formattedSS}:${formattedMS}`;
+}
+
+function print(txt) {
+    document.getElementById("display").innerHTML = txt;
+}
+
+function start() {
+    startTime = Date.now() - elapsedTime;
+    timerInterval = setInterval(() => {
+        elapsedTime = Date.now() - startTime;
+        print(timeToString(elapsedTime));
+    }, 10);
+}
+
+function pause() {
+    clearInterval(timerInterval);
+    participants[currentParticipantIndex].time = elapsedTime;
+    totalElapsedTime += elapsedTime;
+    elapsedTime = 0;
+}
+
+function updateTotalElapsed() {
+    document.getElementById('totalTime').innerHTML = 'Tempo total: ' + timeToString(totalElapsedTime);
+}
+
+function checkEmptyParticipants() {
+    return document.getElementById('participants').value.trim() !== '';
 }
